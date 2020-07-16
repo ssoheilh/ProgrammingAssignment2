8dcfdbb2c3ad2ba57084fdf6852e45175ac7e284
Soheil <ssoheilhosseini@gmail.com>
2020-07-16 19:47:31 +1000

    getting ready to push the edited file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We want to create a code that can compute the inverse of a matrix or 
+## retrieve the inverse if it has already been computed.
 
-## Write a short comment describing this function
+## The following function creates a special matrix object
+## that can store its inverse if it has been already computed.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set_matrix <- function(y){
+        x <<- y
+        i <<- NULL
+    }
+    get_matrix <- function () x
+    set_inverse <- function(inverse) i <<- inverse
+    get_inverse <- function() i
+    list(set_matrix = set_matrix , get_matrix = get_matrix,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a matrix defined by the function
+## above and computes its inverse if it has not been already computed; 
+## otherwise, it will retrieve it form the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$get_inverse()
+    if (!is.null(inv)) {
+        message("Inverse already computed")
+        return(inv)
+    }
+    my_matrix <- x$get_matrix()
+    inv <- solve(my_matrix , ...)
+    x$set_inverse(inv)
+    inv
 }
+
